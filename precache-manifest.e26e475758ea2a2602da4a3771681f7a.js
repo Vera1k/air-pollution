545fcf0d908c9deb8fb69e1c1e202d526f0fb9db
Vera1k <Vera1k@users.noreply.github.com>
2020-10-05 00:43:14 -0500

    Updates

---
+++ precache-manifest.e26e475758ea2a2602da4a3771681f7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b280b6c7f53a2120b3a12a4500885516",
+    "url": "/Xsort-Programming/one-health-approach/index.html"
+  },
+  {
+    "revision": "60f1469f6bfef950488f",
+    "url": "/Xsort-Programming/one-health-approach/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "c6c798435f4a4e841fee",
+    "url": "/Xsort-Programming/one-health-approach/static/css/main.691825a1.chunk.css"
+  },
+  {
+    "revision": "60f1469f6bfef950488f",
+    "url": "/Xsort-Programming/one-health-approach/static/js/2.c7f5ff92.chunk.js"
+  },
+  {
+    "revision": "ebec6ec5880fcec562f9a7747ae47b51",
+    "url": "/Xsort-Programming/one-health-approach/static/js/2.c7f5ff92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6c798435f4a4e841fee",
+    "url": "/Xsort-Programming/one-health-approach/static/js/main.85c5b65b.chunk.js"
+  },
+  {
+    "revision": "6a776c0031679d4eabdc",
+    "url": "/Xsort-Programming/one-health-approach/static/js/runtime-main.c6810edf.js"
+  }
+]);
